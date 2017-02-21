35254dab56c3bb1a348dc430c267002f6a1024c2
Tom Conchie <tconchie@gmail.com>
2017-02-21 15:49:49 -0700

    nice

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-$(document).ready(function(){
-
+$(document).ready(() => {
+  $('button').click(() => {
+    let userSearch = $('#search').val();
+    $.ajax({
+      method: 'GET',
+      url: `http://www.omdbapi.com/?s=${userSearch}`,
+      dataType: 'JSON',
+      success: function(data) {
+        let collections = $('.collection');
+        $(collections).css("display", "block")
+        for (var i = 0; i < 10; i++) {
+          $(collections).append(`<li class="collection-item">${data.Search[i].Title}(${data.Search[i].Year})</li>`);
+        }
+      },
+      error: function() {
+        console.log("error");
+      }
+    });
+  });
 });
